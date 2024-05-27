5ab0aac78dbf3107d49238453d1ea77482095893
Joe Choi <joechoi.newyork@gmail.com>
2024-05-27 19:36:34 -0400

    update

--- app/app.ts
+++ app/app.ts
@@ -2,7 +2,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 // Create a DocumentClient that represents the query to add an item
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBDocumentClient, PutCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
@@ -48,7 +48,6 @@ export const getAllItemsHandler = async (event: APIGatewayProxyEvent) => {
     if (event.httpMethod !== "GET") {
         throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
     }
-    // All log statements are written to CloudWatch
     console.info("received:", event);
 
     // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
@@ -61,30 +60,79 @@ export const getAllItemsHandler = async (event: APIGatewayProxyEvent) => {
     try {
         const data = await ddbDocClient.send(new ScanCommand(params));
         const items = data.Items;
-
         const response = {
             statusCode: 200,
+            /*
             headers: {
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Allow-Origin": "*",
                 "Access-Control-Allow-Methods": "*"
-            },
+            }, */
             body: JSON.stringify(items)
         };
-
-        // All log statements are written to CloudWatch
         console.info(
             `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
         );
         return response;
     } catch (err) {
-        console.log("Error", err);
-
-        const response = {
+        console.error("Error", err);
+        return {
             statusCode: 400,
             body: JSON.stringify(err)
         };
+    }
+};
 
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+export const postItemHandler = async (event: APIGatewayProxyEvent) => {
+    if (event.httpMethod !== "POST") {
+        throw new Error(
+            `putMethod only accepts PUT method, you tried: ${event.httpMethod} method.`
+        );
+    }
+    console.info("received:", event);
+
+    // Get id and name from the body of the request
+    const body = JSON.parse(event?.body?.toString() || "");
+    const id = body.id;
+    const displayName = body?.displayName || "";
+    const description = body?.description || ""; // `testing description ${new Date()}`;
+    const parentIds = body?.parentIds || "";
+    const childIds = body?.childIds || "";
+    const alternateNames = body?.alternateNames || "";
+
+    // Creates a new item, or replaces an old item with a new item
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    const params = {
+        TableName: tableName,
+        Item: {
+            id: id,
+            displayName: displayName,
+            description: description,
+            parentIds: parentIds,
+            childIds: childIds,
+            alternateNames: alternateNames
+        }
+    };
+
+    try {
+        const data = await ddbDocClient.send(new PutCommand(params));
+        console.log("Success - item added or updated", data);
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(body)
+        };
+        console.info(
+            `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+        );
         return response;
+    } catch (err) {
+        console.error("Error", err);
+        return {
+            statusCode: 400,
+            body: JSON.stringify(err)
+        };
     }
 };
