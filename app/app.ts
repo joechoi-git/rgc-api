a56b0ac58c90a0bef4460161616f82b0e18afe2f
Joe Choi <joechoi.newyork@gmail.com>
2024-05-27 16:26:27 -0400

    update

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,14 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
+// Create a DocumentClient that represents the query to add an item
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+// Get the DynamoDB table name from environment variables
+const tableName = "sam-app-SampleTable-1R8VRLJJZASR2"; // process.env.SAMPLE_TABLE;
+
 /**
  *
  * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
@@ -32,24 +41,45 @@ export const lambdaHandler = async (
     }
 };
 
-export const lambdaHandler2 = async (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
+/**
+ * A simple example includes a HTTP get method to get all items from a DynamoDB table.
+ */
+export const getAllItemsHandler = async (event: APIGatewayProxyEvent) => {
+    if (event.httpMethod !== "GET") {
+        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    // All log statements are written to CloudWatch
+    console.info("received:", event);
+
+    // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
+    // https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_Scan.html
+    const params = {
+        TableName: tableName
+    };
+
     try {
-        return {
+        const data = await ddbDocClient.send(new ScanCommand(params));
+        const items = data.Items;
+
+        const response = {
             statusCode: 200,
-            body: JSON.stringify({
-                message: "hello world 2! 5!"
-            })
+            body: JSON.stringify(items)
         };
+
+        // All log statements are written to CloudWatch
+        console.info(
+            `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+        );
+        return response;
     } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "some error happened"
-            })
+        console.log("Error", err);
+
+        const response = {
+            statusCode: 400,
+            body: JSON.stringify(err)
         };
+
+        return response;
     }
 };
