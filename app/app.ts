e2d8d9acea9855be83c08e29ddc531c600fe4a40
Joe Choi <joechoi.newyork@gmail.com>
2024-05-27 21:03:20 -0400

    integration part 2 (#3)
    integrated with the frontend

--- app/app.ts
+++ app/app.ts
@@ -2,54 +2,32 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 // Create a DocumentClient that represents the query to add an item
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, ScanCommand } from "@aws-sdk/lib-dynamodb";
+import {
+    DeleteCommand,
+    DynamoDBDocumentClient,
+    PutCommand,
+    ScanCommand
+} from "@aws-sdk/lib-dynamodb";
 const client = new DynamoDBClient({});
 const ddbDocClient = DynamoDBDocumentClient.from(client);
 
-// Get the DynamoDB table name from environment variables
 const tableName = "sam-app-SampleTable-1R8VRLJJZASR2"; // process.env.SAMPLE_TABLE;
-
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- *
- */
-
-export const lambdaHandler = async (
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    event: APIGatewayProxyEvent
-): Promise<APIGatewayProxyResult> => {
-    try {
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: "hello world! 5!"
-            })
-        };
-    } catch (err) {
-        console.log(err);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({
-                message: "some error happened"
-            })
-        };
-    }
+const headers = {
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*"
 };
 
 /**
  * A simple example includes a HTTP get method to get all items from a DynamoDB table.
  */
-export const getAllItemsHandler = async (event: APIGatewayProxyEvent) => {
+export const getItemsHandler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
     if (event.httpMethod !== "GET") {
-        throw new Error(`getAllItems only accept GET method, you tried: ${event.httpMethod}`);
+        throw new Error(`getItems only accept GET method, you tried: ${event.httpMethod}`);
     }
-    // All log statements are written to CloudWatch
-    console.info("received:", event);
+    console.info("received getItems", event);
 
     // get all items from the table (only first 1MB data, you can use `LastEvaluatedKey` to get the rest of data)
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#scan-property
@@ -61,30 +39,119 @@ export const getAllItemsHandler = async (event: APIGatewayProxyEvent) => {
     try {
         const data = await ddbDocClient.send(new ScanCommand(params));
         const items = data.Items;
-
         const response = {
+            headers: headers,
             statusCode: 200,
-            headers: {
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "*"
-            },
             body: JSON.stringify(items)
         };
-
-        // All log statements are written to CloudWatch
         console.info(
             `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
         );
         return response;
     } catch (err) {
-        console.log("Error", err);
-
-        const response = {
+        console.error("Error", err);
+        return {
+            headers: headers,
             statusCode: 400,
             body: JSON.stringify(err)
         };
+    }
+};
 
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+export const postItemHandler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    if (event.httpMethod !== "POST") {
+        throw new Error(`postItem only accepts PUT method, you tried: ${event.httpMethod} method.`);
+    }
+    console.info("received postItem", event);
+
+    // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+    const body = JSON.parse(event?.body?.toString() || "");
+    const id = body.id;
+    const displayName = body?.displayName || "";
+    const description = body?.description || ""; // `testing description ${new Date()}`;
+    const parentIds = body?.parentIds || "";
+    const childIds = body?.childIds || "";
+    const alternateNames = body?.alternateNames || "";
+    const params = {
+        TableName: tableName,
+        Item: {
+            id: id,
+            displayName: displayName,
+            description: description,
+            parentIds: parentIds,
+            childIds: childIds,
+            alternateNames: alternateNames
+        }
+    };
+
+    try {
+        const data = await ddbDocClient.send(new PutCommand(params));
+        console.log("Success - item added or updated", data);
+        const response = {
+            headers: headers,
+            statusCode: 200,
+            body: JSON.stringify(body)
+        };
+        console.info(
+            `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+        );
         return response;
+    } catch (err) {
+        console.error("Error", err);
+        return {
+            headers: headers,
+            statusCode: 400,
+            body: JSON.stringify(err)
+        };
+    }
+};
+
+/**
+ * A simple example includes a HTTP post method to add one item to a DynamoDB table.
+ */
+export const deleteItemHandler = async (
+    event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+    if (event.httpMethod !== "DELETE") {
+        throw new Error(
+            `deleteItem only accepts DELETE method, you tried: ${event.httpMethod} method.`
+        );
+    }
+    console.info("received deleteItem", event);
+
+    // https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.DeleteItem.html
+    const body = JSON.parse(event?.body?.toString() || "");
+    const id = body.id;
+    const params = {
+        TableName: tableName,
+        Key: {
+            id: id
+        }
+    };
+
+    try {
+        const data = await ddbDocClient.send(new DeleteCommand(params));
+        console.log("Success - item deleted", data);
+        const response = {
+            headers: headers,
+            statusCode: 200,
+            body: JSON.stringify(body)
+        };
+        console.info(
+            `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+        );
+        return response;
+    } catch (err) {
+        console.error("Error", err);
+        return {
+            headers: headers,
+            statusCode: 400,
+            body: JSON.stringify([err, params])
+        };
     }
 };
